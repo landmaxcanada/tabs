cb943a2830da4163ec203105509df0cb802dfe9f
James Swansburg <james@landmax.ca>
2014-11-11 09:37:36 -0700

    added index.html

---
+++ js/tabs.js
@@ -0,0 +1,6 @@
+
+document.querySelector('#mediaQuery').addEventListener('core-media-change',
+  function(e) {
+    document.body.classList.toggle('core-narrow', e.detail.matches);
+    document.querySelector('#scrollableTabs').updateBar();
+});
